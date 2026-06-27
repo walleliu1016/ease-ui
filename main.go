3f3375a0938737e066ae6555a8b037195aba799f
Ease Dev <dev@ease.local>
2026-06-28 01:08:57 +0800

    feat(cli): add ease-ui init subcommand for first-time setup

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/akke/ease-ui/internal/cli"
 )
 
 var version = "dev"
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "init" {
-		// CLI init handled in Task 23
-		fmt.Println("init placeholder")
+		if err := cli.RunInit(); err != nil {
+			fmt.Fprintln(os.Stderr, "ease-ui init:", err)
+			os.Exit(1)
+		}
 		return
 	}
-	fmt.Println("ease-ui", version)
+	fmt.Println("ease-ui", version, "(GUI launch handled by wails build)")
 }
